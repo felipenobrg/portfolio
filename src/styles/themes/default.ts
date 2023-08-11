ac7c1ed29507ddd939ba7f79eb21dd7c918a7a0b
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-10 21:18:19 -0300

    structuring project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  white: "#FFF",
+
+  "gray-100": "#E1E1E6",
+  "gray-300": "#C4C4CC",
+  "gray-400": "#8D8D99",
+  "gray-500": "#7C7C8A",
+  "gray-600": "#323238",
+  "gray-700": "#29292E",
+  "gray-800": "#202024",
+  "gray-900": "#121214",
+
+  "green-500": "#68CF2A",
+
+  "yellow-300": "#faff00",
+  "yellow-500": "#d7df00",
+  "yellow-700": "#b4bf00",
+  "yellow-900": "#FBDC07",
+
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
+
+  "orange-500": "#FBA94C",
+};
