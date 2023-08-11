9cdeb0256703e21e8c4f71a068f266518cc9249d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-11 12:57:02 -0300

    creating section container

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,17 +1,33 @@
 import { styled } from "styled-components";
 
 export const HeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: ${(props) => props.theme["gray-800"]};
-  padding: 3rem;
-  max-height: 10rem;
-  max-width: 100%;
-  z-index: 100;
-
-  position: sticky;
+  position: absolute;
   top: 0;
-
+  width: 100%;
+  z-index: 1;
+  padding: 2rem;
+ 
 
 `;
+
+export const FelipeLogo = styled.div`
+  margin-right: auto;
+  font-family: 'Poppins', sans-serif;
+  font-weight: bold;
+  
+  h1 {
+    font-size: 1.4rem;
+  }
+`
+
+export const LinksContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme["white"]};
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+`
