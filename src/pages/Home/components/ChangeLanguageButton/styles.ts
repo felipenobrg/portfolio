ea05e7c8c92fc14d78a34136997d5ffac970d312
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-18 13:45:35 -0300

    fixing change language button styles error

--- src/pages/Home/components/ChangeLanguageButton/styles.ts
+++ src/pages/Home/components/ChangeLanguageButton/styles.ts
@@ -13,24 +13,28 @@ export const ChangeLanguageContainer = styled(Menubar.Root)`
 `;
 
 export const MenuBarIcon = styled(Menubar.Trigger)`
- background: none;
- border: 0;
-p {
-  display: flex;
-  align-items: center;
-  gap: 0.3rem;
-  background: ${(props) => props.theme["gray-900"]};
-  color: ${(props) => props.theme["white"]};
-  padding: 0.5rem;
+  background: none;
   border: 0;
-  border-radius: 9999px;
-  font-weight: bold;
-  cursor: pointer;
-  width: 8rem;
-  margin-right: auto;
+  p {
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+    background: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["white"]};
+    padding: 0.5rem;
+    border: 0;
+    border-radius: 9999px;
+    font-weight: bold;
+    cursor: pointer;
+    width: 9rem;
+    margin-right: auto;
 
-  max-width: 8rem; 
-}
+    max-width: 10rem;
+
+    svg {
+      flex-shrink: 0; 
+    }
+  }
 `;
 
 export const MenuBarContent = styled(Menubar.Content)`
@@ -60,5 +64,5 @@ export const MenuBarItem = styled(Menubar.Item)`
 
   color: ${(props) => props.theme.white};
   cursor: pointer;
-  margin-top: 0.2rem;  
+  margin-top: 0.2rem;
 `;
