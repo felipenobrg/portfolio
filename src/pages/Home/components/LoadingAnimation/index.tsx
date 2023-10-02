ef3e39798b0720f6c5e87f33ee9a464a268dfd3d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-10-02 18:13:43 -0300

    feat: changing loading style

--- src/pages/Home/components/LoadingAnimation/index.tsx
+++ src/pages/Home/components/LoadingAnimation/index.tsx
@@ -1,9 +1,11 @@
-import { PreloaderContainer, Spinner } from "./styles";
+import { Spinner } from "./styles";
 
 export function LoadingSpinner() {
-    return (
-        <PreloaderContainer>
-        <Spinner />
-      </PreloaderContainer>
-    );
-  }
+  return (
+    <Spinner>
+      <div className="bounce1"></div>
+      <div className="bounce2"></div>
+      <div className="bounce3"></div>
+    </Spinner>
+  );
+}
--- src/pages/Home/components/LoadingAnimation/styles.ts
+++ src/pages/Home/components/LoadingAnimation/styles.ts
@@ -1,27 +1,47 @@
-import styled, { keyframes } from 'styled-components';
-
-const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-export const PreloaderContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: ${(props => props.theme['blue-400'])};
-`;
+import { styled } from "styled-components";
 
 export const Spinner = styled.div`
-  border: 6px solid ${(props => props.theme['blue-300'])};
-  border-top: 6px solid ${(props => props.theme['blue-400'])};
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: ${spin} 2s linear infinite;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: ${(props => props.theme["blue-400"])};
+  width: 100vw;
+  height: 100vh;
+
+ div {
+  width: 18px;
+  height: 18px;
+  background: ${(props => props.theme["blue-300"])};
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+ .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+
+ .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+
+@-webkit-keyframes sk-bouncedelay {
+  0%, 80%, 100% { -webkit-transform: scale(0) }
+  40% { -webkit-transform: scale(1.0) }
+}
+
+@keyframes sk-bouncedelay {
+  0%, 80%, 100% { 
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  } 40% { 
+    -webkit-transform: scale(1.0);
+    transform: scale(1.0);
+  }
+}
+`
