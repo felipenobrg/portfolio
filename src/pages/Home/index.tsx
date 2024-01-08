1b653225a9005c4c095d70cb642e27e3f3c5039c
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-08 17:50:22 -0300

    feat: removing loading spinner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,21 @@
-import { useState, useEffect } from "react";
 import { SectionAbout } from "./components/SectionAbout";
 import { AboutMe } from "./components/AboutMe";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { ChangeLanguageButton } from "./components/ChangeLanguageButton";
 import { Footer } from "./components/Footer";
-import { LoadingSpinner } from "./components/LoadingAnimation";
 import { ContactMe } from "./components/ContactMe";
 
-
 export function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setIsLoading(false);
-      clearTimeout(delay);
-    }, 2000);
-  }, []);
-
   return (
     <>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          <SectionAbout />
-          <AboutMe />
-          <Skills />
-          <Projects />
-          <ContactMe />
-          <ChangeLanguageButton />
-          <Footer />
-        </>
-      )}
+      <SectionAbout />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <ContactMe />
+      <ChangeLanguageButton />
+      <Footer />
     </>
   );
 }
